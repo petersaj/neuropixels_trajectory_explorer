247bb95cb47990b33bc2d82c3e88591703c08007
Andy Peters <peters.andrew.j@gmail.com>
2022-10-19 14:51:28 +0100

    Fixed "set endpoint" function

--- neuropixels_trajectory_explorer.m
+++ neuropixels_trajectory_explorer.m
@@ -581,8 +581,11 @@ new_probe_position = cellfun(@str2num,new_probe_position_input);
 probe_angle_rad = (new_probe_position(4:5)/360)*2*pi;
 
 % Update the probe and trajectory reference
-max_ref_length = norm([max(gui_data.ap_coords);max(gui_data.dv_coords);max(gui_data.ml_coords)]);
-[y,x,z] = sph2cart(pi-probe_angle_rad(1),pi-probe_angle_rad(2),max_ref_length);
+ml_lim = xlim(gui_data.handles.axes_atlas);
+ap_lim = ylim(gui_data.handles.axes_atlas);
+dv_lim = zlim(gui_data.handles.axes_atlas);
+max_ref_length = norm([range(ap_lim);range(dv_lim);range(ml_lim)]);
+[y,x,z] = sph2cart(pi+probe_angle_rad(1),pi-probe_angle_rad(2),max_ref_length);
 
 % Move probe reference (draw line through point and DV 0 with max length)
 probe_ref_top_ap = interp1(new_probe_position(3)+[0,z],new_probe_position(1)+[0,y],0,'linear','extrap');
